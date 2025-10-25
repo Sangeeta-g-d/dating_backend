c14b72464ba9a2ab38e0d4a25de01a8924d74a1d
Sangeeta-g <sangeetadani599@gmail.com>
2025-10-25 09:59:52 +0530

    changed rounting

--- ask_me_feature/routing.py
+++ ask_me_feature/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/askme/(?P<question_id>\d+)/$', consumers.AskMeConsumer.as_asgi()),
-    re_path(r'ws/questions/$', consumers.QuestionConsumer.as_asgi()),
+    re_path(r'^ws/questions/?$', consumers.QuestionConsumer.as_asgi()),
 ]
