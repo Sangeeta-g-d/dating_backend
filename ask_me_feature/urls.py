d5a315723fcac91a9e497f52ff97df3f4e90fe96
Sangeeta-g <sangeetadani599@gmail.com>
2025-11-10 23:43:44 +0530

    minor issue

--- ask_me_feature/urls.py
+++ ask_me_feature/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('ask-question/', AddQuestionAPIView.as_view(), name='ask-question'),
     path('answer-question/<int:question_id>/', AddAnswerAPIView.as_view(),name='answer-question'),
-    path('questions/',QuestionListAPIView.as_view(),"questions"),
+    path('questions/',QuestionListAPIView.as_view(),name="questions"),
     path('view-all-ans/<int:question_id>/',QuestionAnswersAPIView.as_view(),name="view-all-ans")
 
 ]
