0c96d760760cb066d068129a27788c14310dd8c3
Sangeeta-g <sangeetadani599@gmail.com>
2025-11-19 17:14:06 +0530

    Asgi

--- dating_backend/asgi.py
+++ dating_backend/asgi.py
@@ -2,12 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dating_backend.settings")
 
+django_asgi_app = get_asgi_application()
+
+import chat.routing  # safe now, AFTER apps are loaded
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
