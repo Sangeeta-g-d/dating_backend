9d0fc968855b9f28ff5460d7c6889d43f1e890f5
Sangeeta-g <sangeetadani599@gmail.com>
2025-11-19 22:37:12 +0530

    modified chat history API

--- ask_me_feature/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/askme/(?P<question_id>\d+)/$', consumers.AskMeConsumer.as_asgi()),
-    re_path(r'^ws/questions/?$', consumers.QuestionConsumer.as_asgi()),
-]
