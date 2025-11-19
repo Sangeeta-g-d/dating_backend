c63b196125d16f64df3461831ed8ff0311b7b5b7
Sangeeta-g <sangeetadani599@gmail.com>
2025-11-19 23:19:38 +0530

    Added urls

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('chat-history/<int:user_id>/', ChatRoomHistoryAPIView.as_view(), name='chat-history'),
-    path('inbox/',InboxUserListAPIView.as_view(),name="inbox")
+    path('inbox/',InboxUserListAPIView.as_view(),name="inbox"),
+    path('send-media/<int:room_id>/',MediaMessageUploadAPIView.as_view(),name="send-media"),
 ]
